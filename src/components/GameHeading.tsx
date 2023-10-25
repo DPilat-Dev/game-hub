8ce9e45ff9cea639095461a3e7d479ac5bb96d1c
DPilat-Dev <david.j.pilat@gmail.com>
2023-10-25 12:02:23 -0700

    Refactor- Simplifying Query Objects

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -1,13 +1,22 @@
 import { Heading } from "@chakra-ui/react";
 import { GameQuery } from "../App";
+import useGenres from "../hooks/useGenres";
+import usePlatforms from "../hooks/usePlatforms";
 
 interface Props {
   gameQuery: GameQuery;
 }
 
 const GameHeading = ({ gameQuery }: Props) => {
-  const heading = `${gameQuery.platform?.name || ""} ${
-    gameQuery.genre?.name || ""
+  const {data: genres} = useGenres();
+  const {data: platforms} = usePlatforms();
+
+  const genre = genres?.results.find(g => g.id === gameQuery.genreId);
+  const platform = platforms?.results.find(g => g.id === gameQuery.platformId);
+
+
+  const heading = `${platform?.name || ""} ${
+    genre?.name || ""
   } Games`;
 
   return (
