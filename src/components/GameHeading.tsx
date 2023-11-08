4e636ff51d718cd6226552c65603e8a2a62eeaab
DPilat-Dev <david.j.pilat@gmail.com>
2023-11-07 21:33:20 -0800

    Refactor- manage state with Zustand

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -1,17 +1,14 @@
 import { Heading } from "@chakra-ui/react";
-import { GameQuery } from "../App";
-import useGenres from "../hooks/useGenres";
-import usePlatforms from "../hooks/usePlatforms";
-import usePlatfom from "../hooks/usePlatform";
 import useGenre from "../hooks/useGenre";
+import usePlatfom from "../hooks/usePlatform";
+import useGameQueryStore from "../store";
 
-interface Props {
-  gameQuery: GameQuery;
-}
-
-const GameHeading = ({ gameQuery }: Props) => {
-  const genre = useGenre(gameQuery.genreId);
-  const platform = usePlatfom(gameQuery.platformId);
+const GameHeading = () => {
+  const genreId = useGameQueryStore(s => s.gameQuery.genreId);
+  const genre = useGenre(genreId);
+  
+  const platformId = useGameQueryStore(s => s.gameQuery.platformId);
+  const platform = usePlatfom(platformId);
 
   const heading = `${platform?.name || ""} ${
     genre?.name || ""
--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,17 +1,15 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
-import usePlatforms, { Platform } from "../hooks/usePlatforms";
 import usePlatform from "../hooks/usePlatform";
+import usePlatforms from "../hooks/usePlatforms";
+import useGameQueryStore from "../store";
 
-interface Props {
-  onSelectPlatform: (platform: Platform) => void;
-  selectedPlatformId?: number;
-}
-
-const PlatformSelector = ({ onSelectPlatform, selectedPlatformId }: Props) => {
+const PlatformSelector = () => {
   const { data, error } = usePlatforms();
 
-  const selectedPlatform = usePlatform(selectedPlatformId);
+  const setSelectedplatformId = useGameQueryStore(s => s.setPlatformId);
+  const platformId = useGameQueryStore(s => s.gameQuery.platformId);
+  const selectedPlatform = usePlatform(platformId);
 
   if (error) return null;
 
@@ -24,7 +22,7 @@ const PlatformSelector = ({ onSelectPlatform, selectedPlatformId }: Props) => {
         {data?.results.map((platform) => (
           <MenuItem
             key={platform.id}
-            onClick={() => onSelectPlatform(platform)}
+            onClick={() => setSelectedplatformId(platform.id)}
           >
             {platform.name}
           </MenuItem>
