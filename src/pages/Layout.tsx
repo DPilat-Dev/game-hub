540bff1241b82e420b262069e55c40dad888894b
DPilat-Dev <david.j.pilat@gmail.com>
2023-11-21 15:41:05 -0800

    Set Up Routing

---
+++ src/pages/Layout.tsx
@@ -0,0 +1,13 @@
+import Navbar from '../components/Navbar'
+import { Outlet } from 'react-router-dom'
+
+const Layout = () => {
+  return (
+    <>
+        <Navbar />
+        <Outlet />
+    </>
+  )
+}
+
+export default Layout
