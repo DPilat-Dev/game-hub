c2f522ea2d3a7b2456d49c3dc900529e2e465cd6
DPilat-Dev <david.j.pilat@gmail.com>
2023-11-23 16:05:01 -0800

    Build game screenshots component

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -5,6 +5,7 @@ import CriticScore from '../components/CriticScore';
 import DefinitionItem from '../components/DefinitionItem';
 import ExpandableText from '../components/ExpandableText';
 import GameAttributes from '../components/GameAttributes';
+import GameScreenshots from '../components/GameScreenshots';
 import GameTrailer from '../components/GameTrailer';
 import useGame from '../hooks/useGame';
 
@@ -23,6 +24,7 @@ const GameDetailPage = () => {
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAttributes game={game} />
       <GameTrailer gameId={game.id}/>
+      <GameScreenshots gameId={game.id}/>
     </>
   )
 }
